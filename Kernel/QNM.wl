BlackHolePerturbationToolkit/QNM-Mathematica

commit 4f0e3b0f6e0f1dc3b774e8f501811f81cca9c961

Make sure an error message is always printed when interpolation data is not available
Previously, memoisation meant that the message was not printed on
second and subsequent attempts.

Kernel/QNM.wl
@@ -268,11 +268,13 @@ ClearAll[QNMFrequencyInterpolation];
 
 Options[QNMFrequencyInterpolation] = Options[Interpolation];
 
-QNMFrequencyInterpolation[s_, l_, m_, n_, opts:OptionsPattern[]] := QNMFrequencyInterpolation[s, l, m, n] =
+QNMData[file_, dataset_] := QNMData[file, dataset] = Quiet[Import[file, {"Datasets", dataset}, "ComplexKeys"->{"r", "i"}];, {Import::general, Import::noelem, Import::nffil}];
+
+QNMFrequencyInterpolation[s_, l_, m_, n_, opts:OptionsPattern[]] :=
  Module[{h5file, dataset, data, ret},
   h5file = FileNameJoin[{$QNMDataDirectory, "QNM_s"<>ToString[s]<>".h5"}];
   dataset = "/l"<>ToString[l]<>"/m"<>ToString[m]<>"/n"<>ToString[n];
-  Quiet[data = Import[h5file, {"Datasets", dataset}, "ComplexKeys"->{"r", "i"}];, {Import::general, Import::noelem, Import::nffil}];
+  data = QNMData[h5file, dataset];
   If[MatchQ[data, <|"a"->_, "omega"->_|>],
     ret = Interpolation[Transpose[Lookup[data, {"a","omega"}]], opts];,
     Message[QNMFrequency::nointerp, s, l, m, n];

commit f8c4b6e611ce943538aaaadceaaba879f4d056f6

Fix error in interpolating data tables
This was introduced in 4f0e3b0f6e0f1dc3b774e8f501811f81cca9c961.

Kernel/QNM.wl
@@ -321,7 +321,7 @@ ClearAll[QNMFrequencyInterpolation];
 
 Options[QNMFrequencyInterpolation] = Options[Interpolation];
 
-QNMData[file_, dataset_] := QNMData[file, dataset] = Quiet[Import[file, {"Datasets", dataset}, "ComplexKeys"->{"r", "i"}];, {Import::general, Import::noelem, Import::nffil}];
+QNMData[file_, dataset_] := QNMData[file, dataset] = Quiet[Import[file, {"Datasets", dataset}, "ComplexKeys"->{"r", "i"}], {Import::general, Import::noelem, Import::nffil}];
 
 QNMFrequencyInterpolation[s_, l_, m_, n_, opts:OptionsPattern[]] :=
  Module[{h5file, dataset, data, ret},